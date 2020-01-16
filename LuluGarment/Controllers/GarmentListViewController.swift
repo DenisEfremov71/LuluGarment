3b73cc370229711f6ce1235c1a4c7ec922c0f477
Denis Efremov <efremov@novuscom.net>
2020-01-16 12:41:59 -0800

    Initial commit

---
+++ LuluGarment/Controllers/GarmentListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GarmentListViewController.swift
+//  LuluGarment
+//
+//  Created by Denis Efremov on 2020-01-16.
+//  Copyright © 2020 Denis Efremov. All rights reserved.
+//
+
+import UIKit
+
+class GarmentListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
